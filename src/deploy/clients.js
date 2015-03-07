7b0812e70c3082cfd8b322878dfd88c1cd075e5c
Denis Sokolov <denis@sokolov.cc>
2015-03-08 01:10:26 +0200

    Clients: refactor a run command

--- src/deploy/clients.js
+++ src/deploy/clients.js
@@ -5,7 +5,7 @@
  * real FS and SSH.
  */
 
-var spawn = require('child_process').spawn;
+var childProcess = require('child_process');
 var fs = require('fs');
 var path = require('path');
 
@@ -14,10 +14,28 @@ var quote = require('shell-quote').quote;
 var Promise = require('promise');
 
 /* istanbul ignore next */
-var childError = function(code, stderr){
-	var e = new Error(stderr);
-	e.code = code;
-	return e;
+var run = function(args, opts){
+	opts = opts || {};
+	return new Promise(function(resolve, reject){
+		var process = childProcess.spawn.apply(null, args);
+		var stdout = '';
+		var stderr = '';
+		if (opts.stdin) {
+			process.stdin.write(opts.stdin);
+			process.stdin.end();
+		}
+		process.stdout.on('data', function(data){ stdout += data; });
+		process.stderr.on('data', function(data){ stderr += data; });
+		process.on('close', function(code){
+			if (code === 0) {
+				resolve(stdout);
+			} else {
+				var e = new Error(stderr);
+				e.code = code;
+				reject(e);
+			}
+		});
+	});
 };
 
 var api = {};
@@ -42,42 +60,16 @@ api.fs = function(directory){
 api.ssh = function(server){
 	return {
 		read: function(filepath){
-			return new Promise(function(resolve, reject){
-				var read = spawn('ssh', [server, quote(['cat', filepath])]);
-				var stdout = '';
-				var stderr = '';
-				read.stdout.on('data', function(data){ stdout += data; });
-				read.stderr.on('data', function(data){ stderr += data; });
-				read.on('close', function(code){
-					if (code === 0) {
-						resolve(stdout);
-					} else {
-						reject(childError(code, stderr));
-					}
-				});
-			});
+			return run(['ssh', [server, quote(['cat', filepath])]]);
 		},
 		write: function(filepath, filecontents){
-			return new Promise(function(resolve, reject){
-				var remoteCommand = ['cat', '>', filepath];
-				if (filepath.indexOf('/') > -1) {
-					remoteCommand = ['mkdir', '-p', path.dirname(filepath), '&&']
-						.concat(remoteCommand);
-				}
-				var write = spawn('ssh', [server, quote(remoteCommand)]);
-				var stdout = '';
-				var stderr = '';
-				write.stdin.write(filecontents);
-				write.stdin.end();
-				write.stdout.on('data', function(data){ stdout += data; });
-				write.stderr.on('data', function(data){ stderr += data; });
-				write.on('close', function(code){
-					if (code === 0) {
-						resolve(stdout);
-					} else {
-						reject(childError(code, stderr));
-					}
-				});
+			var remoteCommand = ['cat', '>', filepath];
+			if (filepath.indexOf('/') > -1) {
+				remoteCommand = ['mkdir', '-p', path.dirname(filepath), '&&']
+					.concat(remoteCommand);
+			}
+			return run(['ssh', [server, quote(remoteCommand)]], {
+				stdin: filecontents
 			});
 		}
 	};
